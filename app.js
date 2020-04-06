0d0c5c9c0123e7bd1727496e9daaa5750b02f7fb
Mathan <capturesmk@gmail.com>
2020-04-06 17:02:18 +0530

    Adds Dependencies :heavy_plus_sign:

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
-const express = require("express");
-const axios = require('axios');
-const app = express();
-const port = 3000;
+// Require Imports
+const express       = require("express"),
+      axios         = require('axios'),
+      mongoose      = require('mongoose'),
+      bodyParser    = require('body-parser'),
+      passport      = require('passport'),
+      LocalStrategy = require('passport-local'),
+      session       = require('express-session');
 
+// Include MongoDB models
+const User = require('./models/user');
+      
+// Additional Setup
+const app  = express();
+const port = 3000;
 app.set("view engine","ejs")
 app.use(express.static(__dirname+'/public'))
+app.use(bodyParser.urlencoded({extended: true}))
+mongoose.connect("mongodb://localhost/newsapp")
 
+// Express Session Setup
+app.use(session({
+  secret: 'Webapp by Mathan',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+// Passport Setup
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Variable Declarations
 var authors = new Array();
 var titles = new Array();
 var descriptions = new Array();
@@ -13,6 +40,7 @@ var urls = new Array();
 var imgurls = new Array();
 var sources = new Array(); 
 
+// Route Configurations
 app.get('/',function(req,res){
   axios.get('https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=c60dc7c66a474c03ba181227554788ee')
   .then(function (response) {
@@ -26,13 +54,17 @@ app.get('/',function(req,res){
         urls.push(articles[i]["url"])
         imgurls.push(articles[i]["urlToImage"])
     }
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-Type', 'text/html');
     res.render('home',{sources:sources,authors:authors,titles:titles,descriptions:descriptions,urls:urls,imgurls:imgurls})
   })
   .catch(function (error) {
     // handle error
     console.log(error);
   });   
+});
+
+app.get('/register',function(req,res){
+  res.render('register')
 });
 
 app.get('*',function(req,res){
